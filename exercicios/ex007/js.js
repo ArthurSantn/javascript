5af096a7afb42b07d9a35744d15d5f7181d8b34d
Arthur <138633021+ArthurSantn@users.noreply.github.com>
2023-09-13 21:43:29 -0300

    praticando

---
+++ exercicios/ex007/js.js
@@ -0,0 +1,14 @@
+var pais = 'EUA'
+
+
+if (pais == 'Brasil') {
+    console.log(`Vivendo em ${pais}`)
+} else {
+    console.log (`You have to stop speaking in laguages who i don't understand`)
+}
+
+    if (pais == 'Brasil'){
+        console.log(`Você é brasileiro!!`)
+    } else {
+        console.log(`For gringo is more expensive`)
+    }
