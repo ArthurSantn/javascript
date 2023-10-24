1c6611074e24c1d5428c92c9108f8dd4d5e87d59
Arthur <138633021+ArthurSantn@users.noreply.github.com>
2023-10-23 21:20:31 -0300

    update

--- desafios/identificador/script/script.js
+++ desafios/identificador/script/script.js
@@ -9,12 +9,44 @@ function enviar() {
         var sex = document.getElementsByName('optsex')
         var idade = ano - Number(born.value)
         var gênero = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (sex[0].checked) {
             gênero = `um Homem`
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', './imagens/kid-m.png')
+            } else if (idade < 18) {
+                //adolescente
+                img.setAttribute('src', './imagens/jovem-m.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', './imagens/adulto-m.png')
+            } else {
+                //idoso
+                img.setAttribute('src', './imagens/idoso-m.png')
+            }
+            
         } else if (sex[1].checked)  {
             gênero = `uma Mulher`
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src', './imagens/kid-f.png')
+            } else if (idade < 18) {
+                //adolescente
+                img.setAttribute('src', './imagens/jovem-f.png')
+            } else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', './imagens/adulto-f.png')
+            } else {
+                //idoso
+                img.setAttribute('src', './imagens/idoso-f.png')
+            }
         }
+        res.style.textAlign = 'center'
+        res.style.padding = '10px'
         res.innerHTML = `Parece que você é ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
