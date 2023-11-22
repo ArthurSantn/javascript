ea529b02de70c15ec38f665947dff014eac49b03
Arthur <138633021+ArthurSantn@users.noreply.github.com>
2023-11-22 10:47:47 -0300

    update

---
+++ exercicios/ex011/script/script.js
@@ -0,0 +1,18 @@
+function enviar() {
+var start = window.document.getElementById('inicioid')
+var end = window.document.getElementById('fimid')
+var step = window.document.getElementById('passoid')
+var res = window.document.getElementById('container')
+
+if (start,end,step.value.length == 0) {
+    window.alert(`Por favor, preencha todas as caixas de texto`)
+} else {
+    while (start <= end) {
+        res.innerHTML = `${a} U1F449`
+        step + start
+        } res.innerHTML = 'U1F3C1'
+    }
+}
+
+
+
