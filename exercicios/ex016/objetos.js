ee02950aedbd77ad71264443d815ddebcb7a1edb
Arthur Pereira Santana <arthurklotes1@gmail.com>
2024-08-13 21:45:52 -0300

    Forgoted Commit

---
+++ exercicios/ex016/objetos.js
@@ -0,0 +1,8 @@
+let tabela_de_nomes = {nome:'carlos', idade:15, sexo:'M'}
+console.log(tabela_de_nomes)
+
+let cachorro = "cachorro"
+console.log (cachorro)
+
+let cachorro = "pão"
+console.log(cachorro)
